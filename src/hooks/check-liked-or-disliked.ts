611a379336a2293fe13d49e3ef069ddaa8535112
unknown <himanshu@whozzat.com>
2022-02-05 14:00:45 +0530

    add like unlike feature

--- src/hooks/check-liked-or-disliked.ts
+++ src/hooks/check-liked-or-disliked.ts
@@ -1,7 +1,6 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers';
-import { database } from 'firebase-admin';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (options = {}): Hook => {
